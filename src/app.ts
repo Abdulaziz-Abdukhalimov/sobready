47ce97b50d70f3d792dd137f867580224dc72e41
Abdulaziz Abdukhalimov <abdulazizabduhalimov17@gmail.com>
2025-07-14 21:48:33 +0900

    fix: intial Setup continue

---
+++ src/app.ts
@@ -0,0 +1,48 @@
+import express, { urlencoded } from "express";
+import path from "path";
+import morgan from "morgan";
+import { MORGAN_FORMAT } from "./libs/utils/config";
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+import { T } from "./libs/types/common";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_UR),
+  collection: "sessions",
+});
+
+/* 1-Entrance */
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan(MORGAN_FORMAT));
+
+/* 2- Sessions */
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600 * 3,
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(function (req, res, next) {
+  const sessionInstance = req.session as T;
+  res.locals.member = sessionInstance.member;
+  next();
+});
+
+/* 3- Views */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+/** 4- Routers **/
+//app.use("/admin", routerAdmin);
+//app.use("/", router);
+export default app;
